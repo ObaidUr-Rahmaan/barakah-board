68a7e9af0dd35c6039f2fdd3dd2a2ed8f19c127c
Obaid Ur-Rahmaan <ourrahmaan@gmail.com>
2023-12-23 07:22:23 +0000

    Test Basic Implementation of Web Scraper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from 'next/image';
+'use client';
+
+import { scrapeAndStoreJobs } from '@/lib/actions';
 
 export default function Home() {
+  const handleSubmit = async () => {
+    console.log('Scraping');
+    const job = await scrapeAndStoreJobs(
+      'https://www.glassdoor.co.uk/Job/software-engineer-jobs-SRCH_KO0,17.htm?jobTypeIndeed=CF3CP&seniorityType=entrylevel'
+    );
+  };
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='text-5xl font-bold'>Barakah Board Landing Page</div>
+
+      <button onClick={handleSubmit} className='border-amber-900'>
+        Scrape Job
+      </button>
     </main>
   );
 }
